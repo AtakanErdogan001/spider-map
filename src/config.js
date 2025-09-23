a198b77df4c993b44729a50103a481e2fde19c99
Atakan ERDOGAN <111415147+AtakanErdogan001@users.noreply.github.com>
2025-09-23 12:43:08 +0300

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,15 @@
+export const MAPBOX_TOKEN = 'pk.eyJ1IjoiYXRha2FuZSIsImEiOiJjbWNoNGUyNWkwcjFqMmxxdmVnb2tnMWJ4In0.xgo3tCNuq6kVXFYQpoS8PQ';
+
+export const CATEGORY_WEIGHTS = {
+  'Belediye Binaları': 0.5,
+  'Hizmet Binaları': 0.6,
+  'Kültürel Miras': 0.2,
+  'Mezarlıklar': 0.8,
+  'Okullar': 0.4,
+  'Parklar': 0.3,
+  'Raylı Sistem Durakları': 0.7,
+  'Sağlık Kurumları': 0.9,
+  'Stadyumlar': 1.0,
+  'Su Kaynakları': 0.6,
+  'İbadet Alanları': 0.8
+};
